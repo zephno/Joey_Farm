906e31a6f78ec80bf4aa7791378a5c7755495622
Neil Xyrus <neilxyrusp@gmail.com>
2026-09-14 15:51:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    $('.header').height($(window).height());
-  })
+  $('.header').height($(window).height());
+
+  $('#loginForm').on('submit', function(e) {
+    e.preventDefault(); 
+
+    var email = $('#loginEmail').val();
+    var password = $('#loginPassword').val();
+
+    console.log('Login submitted:', { email: email, password: password });
+    alert('Logged in successfully as ' + email);
+
+    $('#loginModal').modal('hide');
+
+    $(this).trigger('reset');
+  });
+});
